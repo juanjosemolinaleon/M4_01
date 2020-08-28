3a106d4312e7e2739663d779a41b6e0f94b2f4d9
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-28 19:21:35 -0400

    M4_02_01

--- app/src/main/java/com/example/m4_01/Main.kt
+++ app/src/main/java/com/example/m4_01/Main.kt
@@ -8,12 +8,14 @@ fun main() {
     // val number4 = 20
     // var name1 = "JJML"
     // val name2 = "Casa"
-    val palabra1 = "Hola soy JJML"
+    // val palabra1 = "Hola soy JJML"
 
-    val concatenado1 ="El saludo es: $palabra1"
-    println(concatenado1)
+    // val concatenado1 ="El saludo es: $palabra1"
+    // println(concatenado1)
 
-    val concatenado2 ="El largo de la palabra es: ${palabra1.replace(" ","").length}"
-    println(concatenado2)
+    // val concatenado2 ="El largo de la palabra es: ${palabra1.replace(" ","").length}"
+    // println(concatenado2)
+
+    
 }
 
