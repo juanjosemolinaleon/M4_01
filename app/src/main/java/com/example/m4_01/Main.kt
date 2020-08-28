569dc1295d5a77b04cd426aa1e19b9cb160fc1f0
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-28 19:22:48 -0400

    M4_02_01

--- app/src/main/java/com/example/m4_01/Main.kt
+++ app/src/main/java/com/example/m4_01/Main.kt
@@ -1,21 +1,8 @@
 package com.example.m4_01
 
 fun main() {
-    // println("Mi primera App en Kotlin")
-    // var number1 : Int = 10
-    // val number2 : Int = 20
-    // var number3 = 10
-    // val number4 = 20
-    // var name1 = "JJML"
-    // val name2 = "Casa"
-    // val palabra1 = "Hola soy JJML"
 
-    // val concatenado1 ="El saludo es: $palabra1"
-    // println(concatenado1)
 
-    // val concatenado2 ="El largo de la palabra es: ${palabra1.replace(" ","").length}"
-    // println(concatenado2)
 
-    
 }
 
