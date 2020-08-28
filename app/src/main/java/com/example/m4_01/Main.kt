7ac44c645ae6be6a22924fdb2091d7014dd419e6
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-27 22:45:42 -0400

    M4_01_01

--- app/src/main/java/com/example/m4_01/Main.kt
+++ app/src/main/java/com/example/m4_01/Main.kt
@@ -2,5 +2,11 @@ package com.example.m4_01
 
 fun main() {
     // println("Mi primera App en Kotlin")
+    var number1 : Int = 10
+    val number2 : Int = 20
+    var number3 = 10
+    val number4 = 20
+    var name1 = "JJML"
+    val name2 = "Casa"
 }
 
