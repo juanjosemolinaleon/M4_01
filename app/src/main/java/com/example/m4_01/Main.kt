64d9018b1dd7a14f3fbe3b54f03896e902b5303b
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-28 19:48:22 -0400

    M4_02_01

--- app/src/main/java/com/example/m4_01/Main.kt
+++ app/src/main/java/com/example/m4_01/Main.kt
@@ -2,7 +2,22 @@ package com.example.m4_01
 
 fun main() {
 
+// Ejercicio 1
+    var mutable = 30
+    val inmutable = "Casa"
+    val numimut = 65.45
 
+    println("$mutable $inmutable $numimut")
+
+    val numberDouble = 3.555433568945237958937634283462894362342436298362
+    println(numberDouble)
+
+
+// Ejercicio 2
+    val valorChar = 'W'
+    val cadenadedatosString = "Prueba de texto largo para ver cómo se comporta"
+    val floatFloat = 43453435.5464534535354
+    println("$valorChar $cadenadedatosString $floatFloat")
 
 }
 
