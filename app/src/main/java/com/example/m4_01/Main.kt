eda69a9c693d8e7197c41b2f972ed860e08cc293
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-28 20:41:18 -0400

    M4_02_01

--- app/src/main/java/com/example/m4_01/Main.kt
+++ app/src/main/java/com/example/m4_01/Main.kt
@@ -19,5 +19,33 @@ fun main() {
     val floatFloat = 43453435.5464534535354
     println("$valorChar $cadenadedatosString $floatFloat")
 
+// Ejercicio 3
+    var aaa = 10
+    var bbb = 33
+    var ccc = 66
+    var ddd = 0
+    ddd = aaa + bbb + ccc
+    println(ddd)
+    aaa = 55
+    ddd = aaa + bbb + ccc
+    println(ddd)
+    var eee = ddd/3
+    println(eee)
+
+    imprimirNombreApellidoEdad("Juan José","Molina", 48)
+    imprimirNombreApellidoEdad("Andrés","Molina", 17)
+    imprimirNombreApellidoEdad("Martín","Molina", 19)
+    imprimirNombreApellidoEdad("Irina","Molina", 9)
 }
 
+fun getNombreCompleto():String{
+    return "Juan José Molina León"
+}
+
+fun getEdad():Int{
+    return 48
+}
+
+fun imprimirNombreApellidoEdad(nombre:String, apellido:String, edad:Int){
+    println("Mi nombre es $nombre $apellido y tengo $edad años")
+}
